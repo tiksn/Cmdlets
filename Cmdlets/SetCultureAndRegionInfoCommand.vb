c450c63428adc30941c4860ef4103f50735276f7
tiksn <personal@tiksn.com>
2016-03-07 13:17:18 -0600

    partial set culture and regi

--- Cmdlets/SetCultureAndRegionInfoCommand.vb
+++ Cmdlets/SetCultureAndRegionInfoCommand.vb
@@ -9,10 +9,32 @@ Public Class SetCultureAndRegionInfoCommand
 	Public Property Name() As String
 
 	<Parameter()>
+	Public Property Parent() As CultureInfo
+
+	<Parameter()>
 	Public Property SourceCultureInfo() As CultureInfo
 
 	<Parameter()>
 	Public Property SourceRegionInfo() As RegionInfo
+
+	<Parameter()>
+	Public Property CultureEnglishName() As String
+
+	<Parameter()>
+	Public Property CultureNativeName() As String
+
+	<Parameter()>
+	Public Property CurrencyEnglishName() As String
+
+	<Parameter()>
+	Public Property CurrencyNativeName() As String
+
+	<Parameter()>
+	Public Property RegionEnglishName() As String
+
+	<Parameter()>
+	Public Property RegionNativeName() As String
+
 
 	Protected Overrides Sub BeginProcessing()
 		Dim builder As CultureAndRegionInfoBuilder = CultureAndRegionInfoDictionary(Name)
@@ -24,5 +46,30 @@ Public Class SetCultureAndRegionInfoCommand
 		If SourceRegionInfo IsNot Nothing Then
 			builder.LoadDataFromRegionInfo(SourceRegionInfo)
 		End If
+
+		If Parent IsNot Nothing Then
+			builder.Parent = Parent
+		End If
+
+		If CultureEnglishName IsNot Nothing Then
+			builder.CultureEnglishName = CultureEnglishName
+		End If
+
+		If CultureNativeName IsNot Nothing Then
+			builder.CultureNativeName = CultureNativeName
+		End If
+
+		If CurrencyNativeName IsNot Nothing Then
+			builder.CurrencyNativeName = CurrencyNativeName
+		End If
+
+		If CultureEnglishName IsNot Nothing Then
+			builder.CultureEnglishName = CultureEnglishName
+		End If
+
+		If RegionNativeName IsNot Nothing Then
+			builder.RegionNativeName = RegionNativeName
+		End If
+
 	End Sub
 End Class
