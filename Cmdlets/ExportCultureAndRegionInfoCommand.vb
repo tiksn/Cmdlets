e68b060f2db923090eb82d0fae203086dffae211
tiksn <personal@tiksn.com>
2016-03-07 08:40:17 -0600

    export culture and region info

---
+++ Cmdlets/ExportCultureAndRegionInfoCommand.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Management.Automation
+
+<Cmdlet("Export", "CultureAndRegionInfo")>
+Public Class ExportCultureAndRegionInfoCommand
+	Inherits CultureAndRegionInfoCommandBase
+
+	<Parameter(Mandatory:=True)>
+	Public Property Name() As String
+
+	<Parameter(Mandatory:=True)>
+	Public Property FilePath() As String
+
+
+	Protected Overrides Sub BeginProcessing()
+		CultureAndRegionInfoDictionary(Name).Save(FilePath)
+	End Sub
+End Class
