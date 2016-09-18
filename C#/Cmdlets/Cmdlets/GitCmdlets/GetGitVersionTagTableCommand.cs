986a6324c850f867ae11debfadd814a2b7274552
Tigran TIKSN Torosyan <personal@tiksn.com>
2016-09-18 09:13:05 +0300

    partial implementation of version tags

---
+++ C#/Cmdlets/Cmdlets/GitCmdlets/GetGitVersionTagTableCommand.cs
@@ -0,0 +1,35 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Linq;
+using System.Management.Automation;
+using TIKSN.Progress;
+
+namespace TIKSN.Cmdlets.GitCmdlets
+{
+    [Cmdlet("Get", "GitVersionTagTable")]
+    public class GetGitVersionTagTableCommand : PSCmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+
+            var repository = new Repository(SessionState.Path.CurrentFileSystemLocation.Path);
+
+            using (var progress = new PowerShellProgress(this, "Searching ...", "Searching for tags"))
+            {
+                IProgress<OperationProgressReport> p = progress;
+
+                var count = repository.Tags.Count();
+
+                for (int i = 0; i < count; i++)
+                {
+                    var tag = repository.Tags.ElementAt(i);
+
+                    WriteVerbose($"{tag.Annotation}");
+
+                    p.Report(new OperationProgressReport(i + 1, count));
+                }
+            }
+        }
+    }
+}
