23cb40067602628da5b05c38211ceae7765ca31d
Tigran TIKSN Torosyan <personal@tiksn.com>
2017-10-28 00:10:30 +0300

    remove VB project

--- Cmdlets/CultureAndRegionInfoCommandBase.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.Collections.Generic
-Imports System.Globalization
-Imports System.Management.Automation
-
-Public Class CultureAndRegionInfoCommandBase
-	Inherits Cmdlet
-
-	Protected Shared CultureAndRegionInfoDictionary As New Dictionary(Of String, CultureAndRegionInfoBuilder)
-End Class
